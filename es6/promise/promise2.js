e817ab27f9955b451281ed579020b88835ac30e6
RojoChen <rojo@RojoChende-MacBook-Pro.local>
2017-11-16 22:45:02 +0800

    added promise code

--- es6/promise/promise2.js
+++ es6/promise/promise2.js
@@ -1,30 +1,33 @@
-const getPromise =() =>{
-// 宣告 promise 建構式
+const getPromise = (ran = 1) => {
+    //new promise
+    //resolve ,reject
     let newPromise = new Promise((resolve, reject) => {
         // 傳入 resolve 與 reject，表示資料成功與失敗
-        let ran = parseInt(Math.random() * 2); // 隨機成功或失敗
+
         console.log('Promise 開始')
         if (ran) {
-        setTimeout(function(){
-            // 3 秒時間後，透過 resolve 來表示完成
-            resolve('3 秒時間(fulfilled)');
-        }, 3000);
+            setTimeout(function () {
+                // 3 秒時間後，透過 resolve 來表示完成
+                resolve({message:'3 秒時間(fulfilled)'});
+            }, 3000);
         } else {
-        // 回傳失敗
-            reject('失敗中的失敗(rejected)')
+            // 回傳失敗
+            reject({message:'失敗中的失敗(rejected)'})
         }
     });
     return newPromise;
 }
-    
-(() =>{
-    let myPromise =  getPromise();
-    myPromise.then((data)=> {
-        // 成功訊息 (需要 3 秒)
-        console.log(data);
-        }).catch((err)=> {
+let processPromis = () => {
+    let myPromise = getPromise(parseInt(Math.random() * 2));
+    myPromise.then((data) => {
+        // 成功訊息 
+        console.log(data.message);
+    }).catch((err) => {
         // 失敗訊息 
-        console.log(err)
-        });
-})();
-    
+        console.log(err.message);
+    });
+    return myPromise;
+}
+processPromis().then((data)=>{
+    console.log("processPromise message:"+data.message);
+})

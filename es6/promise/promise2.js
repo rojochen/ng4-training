c5c34ddb47773d055045f8fbd067b9ceee064501
WS0AC3055NB\roger.chen <roger.chen@cathayholdings.com.tw>
2017-11-16 11:43:09 +0800

    added promise demo

--- es6/promise/promise2.js
+++ es6/promise/promise2.js
@@ -1,19 +1,30 @@
-let getPromise =( i = 1) =>{
-   let promise =  new Promise((resolve, reject) => {
+const getPromise =() =>{
+// 宣告 promise 建構式
+    let newPromise = new Promise((resolve, reject) => {
+        // 傳入 resolve 與 reject，表示資料成功與失敗
+        let ran = parseInt(Math.random() * 2); // 隨機成功或失敗
+        console.log('Promise 開始')
+        if (ran) {
         setTimeout(function(){
-            if(i % 2 == 1){
-                resolve("Success!"); // Yay！非常順利！
-            }else{
-                reject("Error!");
-            }
-        }, 250);
+            // 3 秒時間後，透過 resolve 來表示完成
+            resolve('3 秒時間(fulfilled)');
+        }, 3000);
+        } else {
+        // 回傳失敗
+            reject('失敗中的失敗(rejected)')
+        }
     });
-    return promise;
+    return newPromise;
 }
-
-() => {
-    let promise = getPromise(1);
-    promise.then(()=>{
-
-    })
-}
+    
+(() =>{
+    let myPromise =  getPromise();
+    myPromise.then((data)=> {
+        // 成功訊息 (需要 3 秒)
+        console.log(data);
+        }).catch((err)=> {
+        // 失敗訊息 
+        console.log(err)
+        });
+})();
+    

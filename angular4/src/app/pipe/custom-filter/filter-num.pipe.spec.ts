f0990524b4f769102439c3e74dd7dd2f2f54a61c
Louis <neighborhood0830@gmail.com>
2017-12-11 02:14:42 +0800

    remove angular4 folder.

--- angular4/src/app/pipe/custom-filter/filter-num.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FilterNumPipe } from './filter-num.pipe';
-
-describe('FilterNumPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FilterNumPipe();
-    expect(pipe).toBeTruthy();
-  });
-});

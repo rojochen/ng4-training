b9ee6dc8a52e0988a6802dd3ee06fe64f97bd6e8
RogerChen <jjc.roger@gmail.com>
2017-12-21 00:44:32 +0800

    added demo

--- typescript/ts/namespace/namespace2.ts
+++ typescript/ts/namespace/namespace2.ts
@@ -2,15 +2,15 @@ namespace com {
     export namespace bt{
        export namespace crm{
             export interface Service{
-
+                log():void
             }
-            export let version:string ='0.0.1';
+
         }
     }
 }
 
-//JAVA package style !
-//其它不是很必要這樣
+//JAVA package style ,不推 !
 {
     let service : com.bt.crm.Service = null;
+    service.log();
 }

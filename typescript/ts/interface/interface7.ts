aacb0db1e44bbcf64370f70e925e38873d49ce73
RogerChen <jjc.roger@gmail.com>
2017-12-20 20:11:19 +0800

    added interface code

--- typescript/ts/interface/interface7.ts
+++ typescript/ts/interface/interface7.ts
@@ -1,21 +1,29 @@
-interface ClockConstructor {
-    //constructor interface
-    new (hour: number, minute: number): ClockInterface;
-}
-//Clock的Interface
-interface ClockInterface {
-    tick();
-}
-//實作
-class DigitalClock implements ClockInterface {
-    constructor(h: number, m: number) { 
-        console.log('DigitalClock constructor');
+{
+    interface ClockConstructor {
+        //constructor interface
+        new(hour: number, minute: number): ClockInterface;
     }
-    tick() {
-        console.log("beep beep");
+    //Clock的Interface
+    interface ClockInterface {
+        tick();
     }
-}
-let cc :ClockConstructor;
-//實作interface的constructor
-let clock:DigitalClock = new cc(1, 1);
+    //實作
+    class DigitalClock implements ClockInterface {
+        constructor(h: number, m: number) {
+            console.log('DigitalClock constructor');
+        }
+        tick() {
+            console.log("beep beep");
+        }
+    }
+    let cc: ClockConstructor;
+    //實作interface的constructor
+    let clock: DigitalClock = new cc(1, 1);
+    function checkObject(cc : ClockConstructor ){
+        //interface 參數使用
+        let concreteObject: DigitalClock = new cc(1,3);
+        concreteObject.tick();
+
+    }
  
+}
